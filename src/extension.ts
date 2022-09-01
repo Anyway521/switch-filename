b924fa0c25acccbae369e3ee8f0a6899a3dc71d9
Chris <43142096+Anyway521@users.noreply.github.com>
2022-09-02 02:40:31 +0800

    can switch first line

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,79 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as _ from 'lodash';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+
 	console.log('Congratulations, your extension "switch-filename" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('switch-filename.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from switch-filename!');
+	let disposable = vscode.workspace.onDidRenameFiles(file => {
+		const { files: [{ oldUri, newUri }] } = file;
+		const oldName = getNameFromPath(oldUri.path);
+		const newName = getNameFromPath(newUri.path);
+
+		const config = vscode.workspace.getConfiguration('switch-filename');
+		const { variableStyle: style } = config;
+
+		const oldVar = toVariableStyle(oldName, style);
+		const newVar = toVariableStyle(newName, style);
+		setTimeout(function () {
+			try {
+				let { activeTextEditor } = vscode.window;
+				if (activeTextEditor) {
+					let { document } = activeTextEditor;
+					const n = document.lineCount;
+					const reg = new RegExp(oldVar, 'g');
+					const pp = new RegExp(oldVar);
+					for (let i = 0; i < n; i++) {
+						let lineText = document.lineAt(i);
+
+						if (pp.test(lineText.text)) {
+							// setTimeout(function () {
+							let newText = lineText.text.replace(reg, newVar);
+							activeTextEditor?.edit((edit) => {
+								edit.replace(lineText.range, newText);
+							});
+							document.save();
+							// }, 200);
+						}
+					}
+				}
+
+			} catch (err) {
+				console.log(err);
+			}
+			vscode.window.showInformationMessage(`All variables '${oldVar}' will be switched to '${newVar}'`);
+		}, 200);
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+const enum Style {
+	pascalCase = 'pascalCase',
+	camelCase = 'camelCase',
+	kebabCase = 'kebabCase',
+	snakeCase = 'snakeCase'
+}
+
+const toVariableStyle = (str: string, style: Style) => {
+	switch (style) {
+		case Style.camelCase:
+			return _.camelCase(str);
+		case Style.kebabCase:
+			return _.kebabCase(str);
+		case Style.snakeCase:
+			return _.snakeCase(str);
+		case Style.pascalCase:
+			return _.upperFirst(_.camelCase(str));
+		default:
+			return str;
+	}
+};
+
+const getNameFromPath = (path: string) => {
+	return path.replace(/(.*\/)*([^.]+).*/ig, "$2");
+};
